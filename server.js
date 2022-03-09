cf5e6c9fd342fe3d226886a8b085b5b635b320e7
Juannito Ratovoniony <juannitoratovoniony9@gmail.com>
2022-03-09 14:11:40 +0100

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 //mongoose.set('debug', true);
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
-const uri = 'mongodb+srv://Juannito:Juannito942!>@cluster0.yimno.mongodb.net/assignments?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://Juannito:Juannito942!@cluster0.yimno.mongodb.net/assignments?retryWrites=true&w=majority';
 
 const options = {
   useNewUrlParser: true,
